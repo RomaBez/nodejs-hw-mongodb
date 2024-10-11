c90965c76c724b8a3857b228f4da1a5408db0f9b
RomaBez <bezvuglyak.roman@gmail.com>
2024-10-12 02:38:39 +0300

    swagger added

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -5,14 +5,16 @@ export const addContactValidationScheme = Joi.object({
   name: Joi.string().min(3).max(20).required().messages({
     'string.base': 'Name should be a string',
     'string.min': 'Name should have min {#limit} characters',
-    'string.max': 'name should have max {$limit} characters',
+    'string.max': 'name should have max {#limit} characters',
     'any.required': 'Name is required',
   }),
   phoneNumber: Joi.string().pattern(phoneNumberPattern).required().messages({
     'any.required': 'Phone number is required',
     'string.pattern.base': 'Please amende your phone number',
   }),
-  email: Joi.string(),
+  email: Joi.string().email().messages({
+    'string.email': 'Email must be a valid email address',
+  }),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid(...contactTypeList),
 });
@@ -21,7 +23,7 @@ export const patchContactValidationScheme = Joi.object({
   name: Joi.string().min(3).max(20).messages({
     'string.base': 'Name should be a string',
     'string.min': 'Name should have min {#limit} characters',
-    'string.max': 'name should have max {$limit} characters',
+    'string.max': 'name should have max {#limit} characters',
   }),
   phoneNumber: Joi.string().pattern(phoneNumberPattern).messages({
     'string.pattern.base': 'Please amende your phone number',
